1c0ac88f8bbb27d37d09fcee17c372710d02d262
yunkhngn <rogkhoa.mail@gmail.com>
2023-05-05 17:15:50 +0700

    Final commit

--- src/components/lib/DataConfig.js
+++ src/components/lib/DataConfig.js
@@ -4,7 +4,7 @@ const dc = {
         subheader: "Created and deployed by @yun.khngn",
         image: "https://i.imgur.com/pVxxd8E.jpg",
         content: "Hello mọi người, vậy là tớ sắp ra trường. Hoặc có thể lúc tớ làm xong cái này, tớ ra trường moẹ rồi. Thì tớ cũng chẳng định làm lưu bút đâu vì tớ lười. Nhưng mà một hôm tớ thấy mọi người đăng lắm quá, thôi thì làm một cái cũng ksao. Mà cái này đặc biệt hơn, bởi tớ k dùng google form hay gì hết hè hè.",
-        content2: "Đây sẽ là project cuối cùng của tớ trong cấp ba và cũng là project tớ dùng để mai sau này tớ đi apply cho các công ty =)))) chẳng biết nữa nma nếu tớ có giàu sau này, thì sự hiện diện của mọi ngừi ở hiện tại đều đáng quý cả hehe.",
+        content2: "Đây sẽ là project cuối cùng của tớ trong cấp ba và cũng là project tớ dùng cho mai sau này nữa =)))) chẳng biết nữa nma nếu tớ có giàu sau này, thì sự hiện diện của mọi ngừi ở hiện tại đều đáng quý cả hehe.",
         button1: "Bắt đầu thui",
         button2: "Đọc lại thư của cậu",
     },
